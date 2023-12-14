1c7d21ae83ef2036e584d8014cd8b3909430eca5
Nguyễn Hữu Niên <125124829+NguyenHuuNien@users.noreply.github.com>
2023-12-14 17:11:46 +0700

    Fix file throw

--- Assets/Scripts/Throw.cs
+++ Assets/Scripts/Throw.cs
@@ -7,6 +7,7 @@ public class Throw : MonoBehaviour
     private Vector3 positionPressMouse;
     private Vector3 positionCurrentOfMouse;
     private bool isRotation = false;
+    private float x = 15f ,y = 5f;
     [SerializeField] private Transform prevBullet;
     // Start is called before the first frame update
     void Start()
@@ -16,7 +17,7 @@ public class Throw : MonoBehaviour
 
     void Update()
     {
-        Debug.DrawLine(transform.position,transform.position + transform.up * 2, Color.blue);
+        Debug.DrawLine(transform.position,transform.position + transform.up * 2, Color.red);
         // Press mouse
         if(Input.GetMouseButtonDown(0)){
             positionPressMouse = getPositionMouse();
@@ -25,7 +26,7 @@ public class Throw : MonoBehaviour
         // Hold mouse
         if(isRotation){
             onHoldMouse(); 
-        }
+        }    
         // Out mouse
         if(Input.GetMouseButtonUp(0)){
             isRotation = false;
@@ -44,8 +45,23 @@ public class Throw : MonoBehaviour
         // direction
         transform.up = PointToMouse;
         //force
-        Vector3 force = PointToMouse.normalized * PointToMouse.magnitude / 50;
-        transform.position = new Vector3(transform.position.x - force.x, transform.position.y - force.y, transform.position.z - force.z);
+        //Vector3 force = PointToMouse / 50;
+        //transform.position = new Vector3(transform.position.x - force.x, transform.position.y - force.y, transform.position.z - force.z);
+        Vector3 forceThrow = transform.position - PointToMouse / 50;
+        Debug.Log(forceThrow);
+        if(Mathf.Abs(forceThrow.x) < x && Mathf.Abs(forceThrow.y) < y){
+            transform.position = forceThrow;
+        }else{
+            Debug.Log("X or y qua gioi han roi");
+            if(Mathf.Abs(forceThrow.x) > x){
+                forceThrow.x = x;
+                transform.position = forceThrow;
+            }
+            if(Mathf.Abs(forceThrow.y) > y){
+                forceThrow.y = y;
+                transform.position = forceThrow;
+            }
+        }
     }
     // change position of mouse to world game 
     private Vector3 getPositionMouse(){
