62e187315c218d7379d4f07e593e471e638300ea
Nguyễn Hữu Niên <125124829+NguyenHuuNien@users.noreply.github.com>
2023-12-15 01:12:23 +0700

    van chua xong throw

--- Assets/Scripts/Throw.cs
+++ Assets/Scripts/Throw.cs
@@ -7,9 +7,8 @@ public class Throw : MonoBehaviour
     private Vector3 positionPressMouse;
     private Vector3 positionCurrentOfMouse;
     private bool isRotation = false;
-    private float x = 15f ,y = 5f;
     [SerializeField] private Transform prevBullet;
-    // Start is called before the first frame update
+    
     void Start()
     {
         positionPressMouse = Vector3.zero;
@@ -42,26 +41,15 @@ public class Throw : MonoBehaviour
                 positionPressMouse.y - positionCurrentOfMouse.y,
                 positionPressMouse.z - positionCurrentOfMouse.z
         );
+        // ##Gioi han PointToMouse.##
         // direction
         transform.up = PointToMouse;
         //force
         //Vector3 force = PointToMouse / 50;
         //transform.position = new Vector3(transform.position.x - force.x, transform.position.y - force.y, transform.position.z - force.z);
         Vector3 forceThrow = transform.position - PointToMouse / 50;
-        Debug.Log(forceThrow);
-        if(Mathf.Abs(forceThrow.x) < x && Mathf.Abs(forceThrow.y) < y){
-            transform.position = forceThrow;
-        }else{
-            Debug.Log("X or y qua gioi han roi");
-            if(Mathf.Abs(forceThrow.x) > x){
-                forceThrow.x = x;
-                transform.position = forceThrow;
-            }
-            if(Mathf.Abs(forceThrow.y) > y){
-                forceThrow.y = y;
-                transform.position = forceThrow;
-            }
-        }
+        
+        transform.position = forceThrow;
     }
     // change position of mouse to world game 
     private Vector3 getPositionMouse(){
