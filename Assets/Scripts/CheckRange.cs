c330498a5f8b3fc453c54ffc05d1f6d8299cb2ec
Nguyễn Hữu Niên <125124829+NguyenHuuNien@users.noreply.github.com>
2023-12-21 17:50:14 +0700

    Hoan Thanh Con Ma

--- Assets/Scripts/CheckRange.cs
+++ Assets/Scripts/CheckRange.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class CheckRange : MonoBehaviour
 {
+    [SerializeField] private HumanMoveAuto ma;
     private void OnTriggerStay2D(Collider2D other) {
         if(other.gameObject.tag == "Player"){
             GetComponent<SpriteRenderer>().color = new Color(255.0f/255.0f,171.0f/255.0f,171.0f/255.0f,75.0f/255.0f);
+            ma.phatHien = true;        
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other) {
+        if(other.gameObject.tag == "Player"){
+            GetComponent<SpriteRenderer>().color = new Color(171.0f/255.0f,255.0f/255.0f,171.0f/255.0f,75.0f/255.0f);
+            ma.phatHien = false;
         }
     }
 }
--- Assets/Scripts/HumanMoveAuto.cs
+++ Assets/Scripts/HumanMoveAuto.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class HumanMoveAuto : MonoBehaviour
 {
+    [SerializeField] private Transform player;
     [SerializeField] private GameObject human;
     [SerializeField] private Transform[] ListPointMove;
     [SerializeField] private float speedRun = 10;
@@ -13,46 +14,65 @@ public class HumanMoveAuto : MonoBehaviour
     private int index;
     private float timeIma;
     public bool phatHien = false;
-    private void Start() {
+    private void Start()
+    {
         timeSleep = 0.0f;
         index = 0;
         target = ListPointMove[index].position;
         isMove = true;
-        InvokeRepeating(nameof(randomTimeSleep),8.0f,15.0f);
+        InvokeRepeating(nameof(randomTimeSleep), 8.0f, 15.0f);
         timeIma = 0;
     }
-    private void Update() {
-        if(!phatHien){
+    private void Update()
+    {
+        Debug.Log(phatHien);
+        if (player.position.y <= -9.0f)
+        {
+            phatHien = false;
+        }
+        if (!phatHien)
+        {
             move();
-        }else{
-            
+        }
+        else
+        {
+            target = player.position;
+            human.transform.position = Vector3.MoveTowards(human.transform.position, target, (speedRun + 0.5f) * Time.fixedDeltaTime);
         }
     }
-    private void move(){
-        timeIma += Time.fixedDeltaTime/10;
-        if(timeIma >= timeSleep){
+    private void move()
+    {
+        timeIma += Time.fixedDeltaTime / 10;
+        if (timeIma >= timeSleep)
+        {
             isMove = false;
-            Invoke(nameof(resetMove),timeSleep);
+            Invoke(nameof(resetMove), timeSleep);
             timeIma = 0;
         }
-        if(isMove){
+        if (isMove)
+        {
             MoveHoyNao();
         }
     }
-    private void resetMove(){
+    private void resetMove()
+    {
         isMove = true;
     }
-    private void MoveHoyNao(){
-        if(human.transform.position ==target){
+    private void MoveHoyNao()
+    {
+        if (human.transform.position == target)
+        {
             index++;
-            if(index>=ListPointMove.Length){
+            if (index >= ListPointMove.Length)
+            {
                 index = 0;
             }
             target = ListPointMove[index].position;
         }
-        human.transform.position = Vector3.MoveTowards(human.transform.position,target,speedRun * Time.fixedDeltaTime);
+        human.transform.position = Vector3.MoveTowards(human.transform.position, target, speedRun * Time.fixedDeltaTime);
     }
-    private void randomTimeSleep(){
-        timeSleep = Random.Range(2.0f,5.0f);
+    private void randomTimeSleep()
+    {
+        timeSleep = Random.Range(2.0f, 5.0f);
     }
 }
